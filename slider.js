5d7e9eeb966912d3d90e0333610e8554e02cbe4b
Sapir Chodorov <sapch8@gmail.com>
2016-08-02 19:13:10 +0300

    2st

--- slider.js
+++ slider.js
@@ -1,16 +1,22 @@
-var myIndex = 0;
-carousel();
+document.getElementById("home-btn").addEventListener("click", function() {
+    showSlides();
+})
 
-function carousel() {
+function showSlides() {
+    var slideIndex = 0;
+
     var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 2000); // Change image every 2 seconds
-}/**
- * Created by sapirchodorov on 02/08/2016.
- */
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
