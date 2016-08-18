008708e79698e2ea1a4a47c9e55fe79c34d7f417
Sapir Chodorov <sapch8@gmail.com>
2016-08-18 20:26:56 +0300

    8st

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-/*date
-document.getElementById("para1").innerHTML = formatAMPM();
-
-function formatAMPM() {
-    var d = new Date(),
-        minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
-        hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
-        ampm = d.getHours() >= 12 ? 'pm' : 'am',
-        months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
-        days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-    return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+' '+hours+':'+minutes+ampm;
-}
-*/
-
-
 /*new date*/
 document.getElementById("date_time").innerHTML = date_time();
 
@@ -47,6 +32,7 @@ function date_time()
 }
 /*new date*/
 
+/*menu options*/
 $("#home-btn").click(function () {
     $("#home").show();
     $("#sweets").hide();
@@ -57,6 +43,10 @@ $("#sweets-btn").click(function () {
     $("#sweets").show();
     $("#home").hide();
     $("#food").hide();
+    $(".recipes").hide();
+    $("#sweets-icon").show();
+    $('#popup-cupcake #recipe-item').empty();
+
 });
 
 $("#food-btn").click(function () {
@@ -64,36 +54,26 @@ $("#food-btn").click(function () {
     $("#home").hide();
     $("#sweets").hide();
 });
+/*menu options*/
 
-
-/*
-$("#sweets-btn").on("click", function () {
-    if (displayIsNone($("#sweets-icon"))) {
-        $("#sweets-icon").css("display", "flex");
-    } else {
-
+/*back button*/
+$(".back").click(function (event) {
+    var target = $( event.target );
+    if ( target.is( "button" ) ) {
+        $('#popup-cupcake #recipe-item').empty();
+        target.parent().hide();
+        $("#sweets-icon").show();
     }
-})
+});
+/*back button*/
 
-$("#cupcake").on("click", function () {
-    $("#sweets-icon").css("display", "none");
-    $("#popup-cupcake").css("display", "block");
-})
+/*sweets options*/
+$("#cupcake").click(function () {
+    $("#popup-cupcake").show();
+    $("#sweets-icon").hide();
+});
 
-
-
-function displayIsNone (item) {
-    if (item.css('display') == 'none') {
-        return true;
-    }
-    else
-       return false;
-}*/
-
-
-
-
-
+/*sweets options*/
 
 
 
--- sweets.js
+++ sweets.js
@@ -1,30 +1,108 @@
 var cupcakes = [{
-    title: "עוגת בננה",
+    name: "עוגת בננה",
+    img: "images/banana_bread.jpg",
+    groceries: ["100 גרם שוקולד מריר", "1.5 כוסות חלב", "1/3 כוס סוכר"],
+    description: "לערבב הכל בקערה, לשמן תבנית ולאפות 40 דקות ב-150 מעלות חום"
+}, {
+    name: "אינגליש קייק",
     img: "",
     groceries: ["", "", ""],
     description: ""
 }, {
-    title: "אינגליש קייק",
+    name: "עוגת גזר",
+    img: "images/carrot_cake.jpg",
+    groceries: ["כוס סוכר", "4-5 גזרים גדולים", "2 כוסות קמח תופח"],
+    description: "להקציף ביצים ובהדרגה להוסיף קמח. בסוף לאפות ב-50 מעלות חום כ-40 דקות"
+}, {
+    name: "עוגת גזר",
     img: "",
     groceries: ["", "", ""],
     description: ""
 }, {
-    title: "עוגת גזר",
+    name: "עוגת גזר",
+    img: "",
+    groceries: ["", "", ""],
+    description: ""
+}, {
+    name: "עוגת גזר",
+    img: "",
+    groceries: ["", "", ""],
+    description: ""
+}, {
+    name: "עוגת גזר",
+    img: "",
+    groceries: ["", "", ""],
+    description: ""
+}, {
+    name: "עוגת גזר",
+    img: "",
+    groceries: ["", "", ""],
+    description: ""
+}, {
+    name: "עוגת גזר",
     img: "",
     groceries: ["", "", ""],
     description: ""
 }];
 
-$("#cupcake").click(function () {
-    $('#popup-cupcake #table').empty();
+$(function () {
+    $("#cupcake").click(function () {
+        $('#popup-cupcake #cake-list').empty();
+        var index = 0;
+        cupcakes.forEach(function (obj) {
+            var str = '<div index="' + index + '" class="recipe-cupcakes">' + obj.name + '</div>';
+            $('#popup-cupcake #cake-list').append(str);
+            index++;
+        })
+    });
+
+    $(document).on("click",".recipe-cupcakes",function () {
+        $('#popup-cupcake #recipe-item').empty();
+        $(".recipe-cupcakes").hide();
+        var itemIndex = $(this).attr('index');
+        var x = 0;
+        cupcakes.forEach(function (obj) {
+            if (x == itemIndex) {
+                console.log(cupcakes[x].name);
+                var name = '<h1 class="recipe-title">' + obj.name + '</h1>';
+                $('#popup-cupcake #recipe-item').append(name);
+                var img = '<a><img class="recipe-img" src="' + obj.img +  ' "></a>';
+                $('#popup-cupcake #recipe-item').append(img);
+                var gro = '<p class="recipe-gro"><u>מצרכים:</u><br>' + obj.groceries + '</p>';
+                $('#popup-cupcake #recipe-item').append(gro);
+                var des = '<p class="recipe-des"><u>הוראות הכנה:</u><br>' + obj.description + '</p>';
+                $('#popup-cupcake #recipe-item').append(des);
+            }
+            x++;
+        });
+    });
+})
+
+
+
+
+
+
+
+/*
+
+
+$("#groceries-list").click(function () {
+    $('#popup-cupcake #recipe-item').empty();
     cupcakes.forEach(function (obj) {
-        var str = '<div class="recipe-button">' + obj.title + '</div>';
-        $('#popup-cupcake #table').append(str);
+        var title = '<div class="recipe-item">' + obj.title + '</div>';
+        $('#popup-cupcake #cake-list').append(title);
+        var img = '<div class="recipe-item">' + obj.img + '</div>';
+        $('#popup-cupcake #cake-list').append(img);
+        var groc = '<div class="recipe-item">' + obj.groceries + '</div>';
+        $('#popup-cupcake #cake-list').append(groc);
+        var des = '<div class="recipe-item">' + obj.description + '</div>';
+        $('#popup-cupcake #cake-list').append(des);
     })
     // $('#sweets').css("display", "block");
 });
 
-
+*/
 
 
 
